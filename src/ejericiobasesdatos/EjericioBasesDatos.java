a9fdd44f1d851718173d2730c3f457683da5be09
Luis <luisfrb56@gmail.com>
2018-05-22 10:53:22 +0200

    Base de da

---
+++ src/ejericiobasesdatos/EjericioBasesDatos.java
@@ -0,0 +1,12 @@
+
+package ejericiobasesdatos;
+
+
+public class EjericioBasesDatos {
+
+    public static void main(String[] args) {
+TablaBD base=new TablaBD();
+base.setVisible(true);
+    }
+    
+}
